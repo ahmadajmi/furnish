e88a80e811ea86e506e47bdda70a1b2df450f278
Ahmad Ajmi <ahmad@ahmadajmi.com>
2015-02-02 11:36:28 +0200

    Added browserify and image optimization settings

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,22 @@
-var app = angular.module('Trust', [])
+'use strict';
 
+var app = angular.module('Trust', [])
   .controller('profileController', ['$scope', '$http', function($scope, $http) {
 
     $http.get('data/profile.json').success(function(data){
       $scope.profile = data;
     })
     .error(function(err){
-      console.log(err, '===')
+      console.log(err);
     });
 
   }])
 
   .controller('reviewsController', ['$scope', function($scope) {
     $scope.reviews = [
-      {name: 'Ahmad Ajmi', 'body': 'this is a good book', done:false},
-      {name: 'Eiad Ajmi', 'body': 'this is a good book', done:false},
-      {name: 'Aml Ajmi', 'body': 'this is a good book', done:false}
+    {name: 'Ahmad Ajmi', 'body': 'this is a good book', done:false},
+    {name: 'Eiad Ajmi', 'body': 'this is a good book', done:false},
+    {name: 'Aml Ajmi', 'body': 'this is a good book', done:false}
     ];
   }]);
+
--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,18 @@ var jshint      = require('gulp-jshint');
 var watch       = require('gulp-watch');
 var haml        = require('gulp-haml');
 var sass        = require('gulp-sass');
+var imagemin    = require('gulp-imagemin');
+var pngquant    = require('imagemin-pngquant');
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
+var concat      = require('gulp-concat');
 var reload      = browserSync.reload;
-
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream');
 
 // HAML
 
-gulp.task('haml', function(){
+gulp.task('haml', function() {
   return gulp.src('app/index.haml')
     .pipe(haml())
     .pipe(gulp.dest('./build'));
@@ -31,7 +35,7 @@ gulp.task('sass', function () {
 
 // Lint JavaScript
 
-gulp.task('hint', function(){
+gulp.task('jshint', function(){
   return gulp.src('./app/js/**/*.js')
     .pipe(jshint())
     .pipe(gulp.dest('./build/js'))
@@ -39,9 +43,32 @@ gulp.task('hint', function(){
 });
 
 
+// Browserify
+
+gulp.task('browserify', function() {
+  return browserify('./app/js/app.js')
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
+});
+
+
+// Images Min Task
+
+gulp.task('images', function () {
+  return gulp.src('./app/images/**')
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./build/images'));
+});
+
+
 // Copy bower_components
 
-gulp.task('copy:bower_components', function(){
+gulp.task('copy:bower_components', function() {
   return gulp.src('./app/bower_components/**/*')
     .pipe(gulp.dest('./build/bower_components'));
 });
@@ -49,7 +76,7 @@ gulp.task('copy:bower_components', function(){
 
 // Copy Data
 
-gulp.task('copy:data', function(){
+gulp.task('copy:data', function() {
   return gulp.src('./app/data/**/*')
     .pipe(gulp.dest('./build/data'));
 });
@@ -57,7 +84,7 @@ gulp.task('copy:data', function(){
 
 // Watch Files & Reload
 
-gulp.task('serve', function(){
+gulp.task('serve', function() {
   browserSync({
     server: {
       baseDir: "./build"
@@ -66,26 +93,26 @@ gulp.task('serve', function(){
 
   gulp.watch(['./app/index.haml'], ['haml', reload]);
   gulp.watch('./app/styles/**/*.scss', ['sass', reload]);
-  gulp.watch('./app/js/**/*.js', ['hint', reload]);
+  gulp.watch('./app/js/**/*.js', ['jshint', 'browserify', reload]);
 });
 
 
 // Clean the Build Output Directory
 
-gulp.task('clean', function(){
- del(['build/*']);
+gulp.task('clean', function() {
+  del(['build/*']);
 });
 
 
 // Build
 
-gulp.task('build', ['clean'], function(){
-  runSequence('haml', 'hint', 'sass', 'copy:bower_components', 'copy:data')
+gulp.task('build', ['clean'], function() {
+  runSequence('haml', 'sass', 'images', 'jshint', 'copy:bower_components', 'copy:data')
 });
 
 
 // Gulp Default
 
-gulp.task('default', ['clean'], function(){
+gulp.task('default', ['clean'], function() {
   gulp.start('build');
 });
